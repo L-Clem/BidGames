fe3000ff697ead3ec56f22e0e9a947d8bfa1a1d3
annaty <tylkowskanna@gmail.com>
2021-12-05 00:41:53 +0100

    setup eslint, added test react component to twig template

--- assets/app.js
+++ assets/app.js
@@ -4,3 +4,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// class Test extends React.Component {
+//     render() {
+//         return <h2>hello there <span>memememe</span></h2>;
+//     }
+// }
+
+// console.log(<Test/>)
+
+// ReactDOM.render(<Test />, document.getElementById('testid'));
--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,10 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('test','./assets/test.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
+    // .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
