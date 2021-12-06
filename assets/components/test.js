d957ac3d440496aea639d7f33b4dab12ddcd9109
annaty <tylkowskanna@gmail.com>
2021-12-06 11:22:27 +0100

    created component that fetches api data

---
+++ assets/components/test.js
@@ -0,0 +1,92 @@
+import React, { Component, useEffect, useState } from "react";
+
+export default class Testcomp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          items: []
+        };
+      }
+
+    componentDidMount() {
+
+        fetch('http://localhost:8000/api/departments', {
+            method: 'GET',
+            headers: {'accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(
+        data => {
+            this.setState({
+            isLoaded: true,
+            items: data
+            });  
+        },
+
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+            this.setState({
+            isLoaded: true,
+            error
+            });
+            }
+        )
+        
+    } 
+
+    render() {
+        const { error, isLoaded, items } = this.state;
+        if (error) {
+          return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+          return <div>Loading...</div>;
+        } else {
+          return (
+              <ul>
+              {items.map(item => (
+                <li key={item.id}>
+                  {item.name}
+                </li>
+              ))}
+            </ul>
+          );
+        }
+      }
+}
+
+// componentDidMount() {
+//     const [data, setData] = useState([]);
+
+//     fetch('http://localhost:8000/api/departments', {
+//         method: 'GET',
+//         headers: {'accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         }
+//     })
+//     .then(res => res.json())
+//     .then(
+//     (result) => {
+//         this.setState({
+//         isLoaded: true,
+//         items: result
+//         });  
+//     },
+
+//     // Note: it's important to handle errors here
+//     // instead of a catch() block so that we don't swallow
+//     // exceptions from actual bugs in components.
+//     (error) => {
+//         this.setState({
+//         isLoaded: true,
+//         error
+//         });
+//         }
+//     )
+    
+// }   
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,6 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
-    .addEntry('test','./assets/test.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     // .enableStimulusBridge('./assets/controllers.json')
