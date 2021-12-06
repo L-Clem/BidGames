f30b6b076e711e3a5f6da381eac7aa855673cde3
annaty <tylkowskanna@gmail.com>
2021-12-06 11:22:27 +0100

    created component that fetches api data

--- assets/app.js
+++ assets/app.js
@@ -2,17 +2,6 @@ import './styles/app.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import Testcomp from './components/test.js'
 
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// class Test extends React.Component {
-//     render() {
-//         return <h2>hello there <span>memememe</span></h2>;
-//     }
-// }
-
-// console.log(<Test/>)
-
-// ReactDOM.render(<Test />, document.getElementById('testid'));
+ReactDOM.render(<Testcomp />, document.getElementById('testid'));
