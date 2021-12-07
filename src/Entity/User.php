6a0bf8e08ffe8cf856cbf80ca12ecde562a8d6c6
L-Clem <japanclems287@gmail.com>
2021-12-07 14:55:02 +0100

    ✨ (filters) : Add filters to all endpoints

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,8 +2,11 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Annotation\ApiSubresource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -52,6 +55,8 @@ use Symfony\Component\Serializer\Annotation\Groups;
     ],
 
 )]
+#[ApiFilter(OrderFilter::class, properties: ['id' => 'ASC', 'lastname' => 'ASC', 'verified' => 'ASC'], arguments: ['orderParameterName' => 'order'])]
+#[ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'lastname' => 'partial'])]
 class User extends Individual
 {
 
